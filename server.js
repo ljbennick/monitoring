7998be434441afa62c6803ca3a7835bb2bcd9eef
Jaxon Bennick <ljbennick5@gmail.com>
2021-08-31 12:35:17 -0600

    changed html

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const students = []
 const app = express()
 app.use(express.json())
 
+app.ust('./style', express.static('./public/styles.css'))
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -21,13 +23,21 @@ app.get('/', (req, res) => {
 app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
-    
-    students.push(name)
-    
-    rollbar.log('Student added successfully', {author: 'Jaxon', type: 'manual entry'})
-    
-    
-    res.status(200).send(students)
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Jaxon'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('Student already exists')
+        res.status(400).send('that student already exists')
+    }
+
 })
 
 const port = process.env.PORT || 4545
