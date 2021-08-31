d50129e69386696323adb48ba75e1e698e066a3b
Jaxon Bennick <ljbennick5@gmail.com>
2021-08-31 11:40:51 -0600

    added rollbar

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
+
+const rollbar = new Rollbar({
+    accessToken: 'aedf3b95ee864207b0158f6d315f8d85',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully.')
 })
 
 const port = process.env.PORT || 4545
